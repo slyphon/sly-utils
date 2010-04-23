fa4b32b797e4b48074a207080548900a0c968cf9
slyphon <slyphon@motionbox.com>
2010-04-23 23:05:36 +0000

    initial import

---
+++ src/com/slyphon/utils/core.clj
@@ -0,0 +1,1 @@
+(ns com.slyphon.utils.core)
---
+++ test/com/slyphon/utils/core_test.clj
@@ -0,0 +1,6 @@
+(ns com.slyphon.utils.core-test
+  (:use [com.slyphon.utils.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
